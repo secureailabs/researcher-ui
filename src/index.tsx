3e2d64da7d1d4ff2c8eab590ce663b217ecd6f82
Aman Trigunait <amantrigunait.iitk@gmail.com>
2023-03-24 02:16:28 +0530

    fix: eslint fixes in default cra

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import reportWebVitals from 'src/reportWebVitals';
 import App from './App';
 import './App.css';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
