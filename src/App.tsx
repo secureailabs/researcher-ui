414d92b6c2abc724564f9ef9f2b11714cfa7a5fb
Aman Trigunait <amantrigunait.iitk@gmail.com>
2023-03-24 03:18:22 +0530

    feat: basic theme and styling setup for mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { createTheme, CssBaseline, StyledEngineProvider } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import GetDesignTokens from 'src/styles/theme';
 import './App.css';
 import Routes from './routes';
 
 // ==============================|| APP ||============================== //
 
+const mode = 'light'; // currently only light mode is supported
+const theme = createTheme(GetDesignTokens(mode));
+
 const App = (): JSX.Element => {
   return (
+    <StyledEngineProvider injectFirst>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
       <Routes />
+    </ThemeProvider>
+    </StyledEngineProvider>
   );
 };
 
---
+++ src/styles/styles.ts
@@ -0,0 +1,23 @@
+const Styles = {
+  light: {
+    // palette values for light mode
+    primary: {
+      light: '#293545',
+      main: '#1A2432',
+      dark: '#1A2432'
+    },
+    default: {
+      light: '#7CFC00',
+      main: '#458B00',
+      dark: '#3B5323'
+    },
+    background: {
+      default: '#f8f8f8'
+    },
+    text: {
+      primary: '#282828'
+    }
+  }
+};
+
+export default Styles;
