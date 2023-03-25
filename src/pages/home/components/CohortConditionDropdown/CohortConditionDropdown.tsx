8277fd2bd381638a1954ecbfb621b1c5ab182ae6
Aman Trigunait <amantrigunait.iitk@gmail.com>
2023-03-25 18:08:09 +0530

    feat: added delete cohort filter functionality

--- src/pages/home/components/CohortConditionDropdown/CohortConditionDropdown.tsx
+++ src/pages/home/components/CohortConditionDropdown/CohortConditionDropdown.tsx
@@ -2,6 +2,7 @@
 import {
   Autocomplete,
   Box,
+  Button,
   FormControl,
   InputLabel,
   MenuItem,
@@ -14,7 +15,11 @@ import CONDITION_LIST from 'src/constants/comparisionList';
 import FEATURE_LIST from 'src/constants/featureVariable';
 import styles from './CohortConditionDropdown.module.css';
 
-export interface ICohortConditionDropdown {}
+export interface ICohortConditionDropdown {
+  filter: any;
+  handleFilterChange: (data: any) => void;
+  handleDeleteFilter: (id: number) => void;
+}
 
 interface AutocompleteOptionData {
   variableName: string;
@@ -33,13 +38,25 @@ const StyledOption = styled('div')<StyledOptionProps>(({ option }) => ({
   wordBreak: 'break-word',
 }));
 
-const CohortConditionDropdown: React.FC<ICohortConditionDropdown> = () => {
+const CohortConditionDropdown: React.FC<ICohortConditionDropdown> = ({
+  filter,
+  handleFilterChange,
+  handleDeleteFilter,
+}) => {
   // custom render option : Implemented to handle long text wrapping in dropdown
   const renderOption = (props: any, option: any): JSX.Element => (
     <li {...props} key={option.variableName}>
       <StyledOption option={option}>{option.variableName}</StyledOption>
     </li>
   );
+
+  const handleFilterDataChange = (data: any): void => {
+    const newFilter = {
+      ...filter,
+      ...data,
+    };
+    handleFilterChange(newFilter);
+  };
 
   return (
     <Box className={styles.container}>
@@ -53,15 +70,27 @@ const CohortConditionDropdown: React.FC<ICohortConditionDropdown> = () => {
           }
           renderInput={(params) => <TextField {...params} label="Feature" />}
           renderOption={renderOption}
+          onChange={(event, newValue) => {
+            if (newValue != null) {
+              handleFilterDataChange({ variable: newValue?.variableName });
+            } else {
+              handleFilterDataChange({ variable: '' });
+            }
+          }}
         />
       </Box>
       <Box className={styles.conditionContainer}>
         <FormControl fullWidth>
           <InputLabel id="demo-simple-select-label">Condition</InputLabel>
           <Select
-            labelId="comparison-condition"
-            id="comparison-condition"
-            label="comparison"
+            labelId="operator"
+            id="operator"
+            label="operator"
+            defaultValue=""
+            value={filter.operator}
+            onChange={(event) => {
+              handleFilterDataChange({ operator: event.target.value });
+            }}
           >
             {CONDITION_LIST.map((condition) => (
               <MenuItem key={condition.value} value={condition.value}>
@@ -72,7 +101,25 @@ const CohortConditionDropdown: React.FC<ICohortConditionDropdown> = () => {
         </FormControl>
       </Box>
       <Box className={styles.valueContainer}>
-        <TextField id="outlined-basic" label="Value" variant="outlined" />
+        <TextField
+          id="outlined-basic"
+          label="Value"
+          variant="outlined"
+          onChange={(event) => {
+            handleFilterDataChange({ value: event.target.value });
+          }}
+        />
+      </Box>
+      <Box className={styles.deleteButton}>
+        <Button
+          variant="text"
+          color="error"
+          onClick={() => {
+            handleDeleteFilter(filter.id);
+          }}
+        >
+          Delete
+        </Button>
       </Box>
     </Box>
   );
--- src/pages/home/components/CohortSelection/CohortSelection.tsx
+++ src/pages/home/components/CohortSelection/CohortSelection.tsx
@@ -5,8 +5,36 @@ import styles from './CohortSelection.module.css';
 
 export interface ICohortSelection {}
 
+interface IFilter {
+  id: number;
+  variable: string;
+  operator: string;
+  value: string;
+}
+
 const CohortSelection: React.FC<ICohortSelection> = () => {
-  const [conditionList, setConditionList] = useState<any>([]);
+  const [filters, setFilters] = useState<IFilter[]>([]);
+
+  const handleAddFilter = (): void => {
+    const lastFilterId =
+      filters.length > 0 ? filters[filters.length - 1].id : 0;
+    const newFilterId = lastFilterId + 1;
+    const newFilterState = [
+      ...filters,
+      {
+        id: newFilterId,
+        variable: '',
+        operator: '',
+        value: '',
+      },
+    ];
+    setFilters(newFilterState);
+  };
+
+  const handleDeleteFilter = (id: number): void => {
+    const newFilterState = filters.filter((f) => f.id !== id);
+    setFilters(newFilterState);
+  };
 
   return (
     <Box className={styles.container}>
@@ -17,10 +45,29 @@ const CohortSelection: React.FC<ICohortSelection> = () => {
           width: '100%',
         }}
       >
-        <CohortConditionDropdown />
+        {filters.map((filter: any) => (
+          <CohortConditionDropdown
+            key={filter.id}
+            filter={filter}
+            handleDeleteFilter={handleDeleteFilter}
+            handleFilterChange={(data: any) => {
+              const newFilterState = filters.map((f: any) => {
+                if (f.id === filter.id) {
+                  return data;
+                }
+                return f;
+              });
+              setFilters(newFilterState);
+            }}
+          />
+        ))}
       </Box>
       <Box>
-        <Button variant="contained" sx={{ marginTop: 2 }}>
+        <Button
+          variant="contained"
+          sx={{ marginTop: 2 }}
+          onClick={handleAddFilter}
+        >
           Add Condition
         </Button>
       </Box>
