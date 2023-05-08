87b747a7826e7ed85f846cc1f2de61fadf2bf783
Aman Trigunait <amantrig@Amans-MacBook-Pro.local>
2023-05-08 12:02:20 +0530

    feat: updated longitudingal page

--- src/pages/DemoLongitudinal/DemoLongitudinal.tsx
+++ src/pages/DemoLongitudinal/DemoLongitudinal.tsx
@@ -1,20 +1,64 @@
 import { useState } from 'react';
-import { Grid } from '@mui/material';
+import { AppBar, Tabs, Tab, Box, Typography } from '@mui/material';
+import { styled } from '@mui/material/styles';
 import SectionCard from 'src/components/card/sectioncard';
 import CohortSelection from './components/CohortSelection';
 import FeatureExtraction from './components/FeatureExtraction';
 import Analysis from './components/Analysis';
+import { IconList, IconUsersGroup, IconChartInfographic } from '@tabler/icons-react';
 
 // types
-import {
-  type TOperatorString,
-  type IFilter,
-} from 'src/shared/types/customTypes';
+import { type TOperatorString, type IFilter } from 'src/shared/types/customTypes';
 
 // styles
 import styles from './DemoLongitudinal.module.css';
 
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div role="tabpanel" hidden={value !== index} id={`simple-tabpanel-${index}`} aria-labelledby={`simple-tab-${index}`} {...other}>
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  };
+}
+
+const StyledTabs = styled(Tabs)`
+  border-radius: ${({ theme }) => theme.spacing(2)}px;
+  overflow: hidden;
+`;
+
+const StyledTab = styled(Tab)(({ theme }) => ({
+  minWidth: 0,
+  backgroundColor: '#f5f5f5',
+  margin: '10px',
+  color: '#000',
+  // if active tab then change color
+  '&.Mui-selected': {
+    backgroundColor: theme.palette.primary.lighter,
+    fontWeight: 'bold'
+  }
+}));
+
 const DemoLongitudinal: React.FC = () => {
+  const [value, setValue] = useState(0);
   const [filters, setFilters] = useState<IFilter[]>([]);
   const [filterOperator, setFilterOperator] = useState<TOperatorString[]>([]);
 
@@ -26,30 +70,29 @@ const DemoLongitudinal: React.FC = () => {
     setFilterOperator(data);
   };
 
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
   return (
-    <Grid container spacing={2} className={styles.container}>
-      {/* Feature Extraction */}
-      <Grid item xs={12}>
-        <SectionCard>
-          <FeatureExtraction />
-        </SectionCard>
-      </Grid>
-      {/* Cohort Selection */}
-      <Grid item xs={12}>
-        <SectionCard>
-          <CohortSelection
-            handleChildFilterChange={handleFilterChange}
-            handleChildFilterOperatorChange={handleFilterOperatorChange}
-          />
-        </SectionCard>
-      </Grid>
-      {/* Analysis */}
-      <Grid item xs={12}>
-        <SectionCard>
-          <Analysis filters={filters} filterOperator={filterOperator} />
-        </SectionCard>
-      </Grid>
-    </Grid>
+    <Box sx={{ width: '100%' }} className={styles.container}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider', width: '100%' }}>
+        <StyledTabs value={value} onChange={handleChange} aria-label="basic tabs example" variant="fullWidth">
+          <StyledTab icon={<IconList />} label="Feature Selection" {...a11yProps(0)} />
+          <StyledTab icon={<IconUsersGroup />} label="Cohort Selection" {...a11yProps(1)} />
+          <StyledTab icon={<IconChartInfographic />} label="Analysis" {...a11yProps(2)} />
+        </StyledTabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <FeatureExtraction />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <CohortSelection handleChildFilterChange={handleFilterChange} handleChildFilterOperatorChange={handleFilterOperatorChange} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <Analysis filters={filters} filterOperator={filterOperator} />
+      </TabPanel>
+    </Box>
   );
 };
 
