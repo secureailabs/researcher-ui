d92f1b9c7d62b84a50cece1a8c33dc468d1f3e08
jessicawormald <jessica.wormald1172@gmail.com>
2023-05-15 10:43:33 -0400

    feat: add validation to login fields

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -6,34 +6,44 @@ import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import { Divider, Stack } from "@mui/material";
+import { Divider, IconButton, InputAdornment, Stack } from "@mui/material";
 import Socials from "./components/Socials";
-
+import styles from './Login.module.css';
+import { Controller, useForm } from "react-hook-form";
+import { useState } from "react";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 
 export interface ILogin {
   sampleTextProp: string;
 }
 
 const Login: React.FC<ILogin> = ({ sampleTextProp }) => {
+  const { handleSubmit, control } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+  const [showPassword, setShowPassword] = useState(false);
+  
+  const handleShowPassword = () => {
+    setShowPassword(prev => !prev);
+  }
+
 
   return (
-    <Container component="main">
+    <Container component="main" className={styles.container}>
       <Box
         sx={{
-          marginTop: 6,
+          mt: 6,
+          width: "100%",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
         }}
       >
-        <Box component="form" noValidate sx={{ mt: 1 }}>
-          <Stack
-            direction="row"
-            alignItems="center"
-            justifyContent="space-between"
+        <Box component="form" noValidate sx={{ mt: 1, width: 400 }} onSubmit={handleSubmit(onSubmit)}>
+          <Box
+            className={styles.stack}
             sx={{ my: 3 }}
           >
-            <Typography component="h1" variant="h3">
+            <Typography variant="h3">
               Login
             </Typography>
             <Link
@@ -43,32 +53,65 @@ const Login: React.FC<ILogin> = ({ sampleTextProp }) => {
             >
               Don't have an account?
             </Link>
-          </Stack>
+          </Box>
           <Typography variant="h6">
             Email Address
           </Typography>
-          <TextField
-            sx={{ mt: 1, mb: 4 }}
-            required
-            fullWidth
-            id="email"
-            label="Enter email address"
+          <Controller
             name="email"
-            autoComplete="email"
-            autoFocus
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <TextField
+                sx={{ mt: 1, mb: 4 }}
+                value={value}
+                onChange={onChange}
+                error={!!error}
+                helperText={error ? error.message : null}
+                required
+                fullWidth
+                id="email"
+                label="Enter email address"
+                autoComplete="email"
+                autoFocus
+              />
+            )}
+            rules={{ required: 'Email address is required' }}
           />
           <Typography variant="h6">
             Password
           </Typography>
-          <TextField
-            sx={{ mt: 1, mb: 2 }}
-            required
-            fullWidth
+          <Controller
             name="password"
-            label="Enter password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
+            control={control}
+            defaultValue=""
+            render={({ field: { onChange, value }, fieldState: { error } }) => (
+              <TextField
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <IconButton
+                        onClick={handleShowPassword}
+                        edge="end"
+                      >
+                        {showPassword ? <Visibility /> : <VisibilityOff />}
+                      </IconButton>
+                    </InputAdornment>)
+                }}
+                sx={{ mt: 1, mb: 2 }}
+                value={value}
+                onChange={onChange}
+                error={!!error}
+                helperText={error ? error.message : null}
+                required
+                fullWidth
+                id="password"
+                label="Enter password"
+                type={showPassword ? "text" : "password"}
+                autoComplete="current-password"
+              />
+            )}
+            rules={{ required: 'Password is required' }}
           />
           <Stack
             direction="row"
@@ -77,7 +120,7 @@ const Login: React.FC<ILogin> = ({ sampleTextProp }) => {
             sx={{ my: 2 }}
           >
             <FormControlLabel
-              control={<Checkbox value="store_signin" color="primary" />}
+              control={<Checkbox value="stay_signedin" color="primary" />}
               label="Keep me signed in" />
             <Link
               variant="subtitle2"
@@ -88,10 +131,11 @@ const Login: React.FC<ILogin> = ({ sampleTextProp }) => {
             </Link>
           </Stack>
           <Button
+            onClick={handleSubmit(onSubmit)}
             type="submit"
             fullWidth
             variant="contained"
-            sx={{ mt: 3, mb: 2 }}
+            sx={{ mb: 2 }}
           >
             Log In
           </Button>
--- src/pages/Login/components/Socials/Socials.tsx
+++ src/pages/Login/components/Socials/Socials.tsx
@@ -1,8 +1,5 @@
 import { Button, Stack } from "@mui/material";
 import GoogleIcon from '@mui/icons-material/Google';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import styles from './Socials.module.css';
 
 const Socials = () => {
 
@@ -19,30 +16,6 @@ const Socials = () => {
           startIcon={<GoogleIcon />}
         >
           Google
-        </Button>
-        <Button
-          sx={{
-            border: "2px solid #ccc",
-            borderRadius: "5px",
-            padding: "0.7rem",
-            flex: 1,
-          }}
-          variant="outlined"
-          startIcon={<TwitterIcon />}
-        >
-          Twitter
-        </Button>
-        <Button
-          sx={{
-            border: "2px solid #ccc",
-            borderRadius: "5px",
-            padding: "0.7rem",
-            flex: 1,
-          }}
-          variant="outlined"
-          startIcon={<FacebookIcon />}
-        >
-          Facebook
         </Button>
       </Stack>
     );
