8aac768993cb49ba2aeb05143f1e9a31903d41a2
jessicawormald <jessica.wormald1172@gmail.com>
2023-06-05 09:52:19 -0400

    feat: datagrid and styles

--- src/pages/Datasets/components/DatasetsTable/DatasetsTable.tsx
+++ src/pages/Datasets/components/DatasetsTable/DatasetsTable.tsx
@@ -1,77 +1,70 @@
+import { GridColDef } from '@mui/x-data-grid';
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import styles from './DatasetsTable.module.css';
 import { useQuery } from 'react-query';
-//import { ApiError } from 'src/client';
-//import { DefaultService, GetMultipleDataset_Out } from 'src/client';
+import { ApiError } from 'src/client';
+import { DefaultService, GetMultipleDataset_Out } from 'src/client';
+import DataGridTable from 'src/components/datagrid/DatagridTable';
+// import styles from './DatasetsTable.module.css';
 
-
-
-function createData(
-  name: string,
-  publishDate: string,
-  keywords: string[],
-  format: string,
-  state: string,
-) {
-  return { name, publishDate, keywords, format, state };
-}
-
-const rows = [
-  createData('data1', '2010', ['key'], '24', 'state'),
-  createData('data1', '2010', ['key'], '24', 'state'),
-  createData('data1', '2010', ['key'], '24', 'state'),
-  createData('data1', '2010', ['key'], '24', 'state'),
-  createData('data1', '2010', ['key'], '24', 'state'),
-];
 
 const DatasetsTable: React.FC = () => {
 
- /* const { data, isLoading, status, error, refetch } = useQuery<
+  const { data } = useQuery<
     GetMultipleDataset_Out,
     ApiError
   >(['datasets'], DefaultService.getAllDatasets, { refetchOnMount: 'always' });
-*/
- 
-  return (
 
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} className={styles.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Publish Date</TableCell>
-            <TableCell align="right">Keywords</TableCell>
-            <TableCell align="right">Format</TableCell>
-            <TableCell align="right">State</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="right">{row.publishDate}</TableCell>
-              <TableCell align="right">{row.keywords}</TableCell>
-              <TableCell align="right">{row.format}</TableCell>
-              <TableCell align="right">{row.state}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-   );
-  };
-  
-  export default DatasetsTable;
-  
+  console.log(data);
+
+  const rows = [
+    { id: 1, name: 'name1', publishDate: '2023-05-30T12:57:18.535811', keywords: ['key1', 'key2'], format: 'format', state: 'state' },
+    { id: 2, name: 'name2', publishDate: '2023-05-30T12:57:18.535811', keywords: 'key2', format: 'csv', state: 'state' },
+    { id: 3, name: 'name3', publishDate: '2023-05-30T12:57:18.535811', keywords: 'key3', format: 'json', state: 'state' },
+    { id: 4, name: 'name4', publishDate: '2023-05-30T12:57:18.535811', keywords: 'key4', format: 'csv', state: 'state' },
+    { id: 5, name: 'name5', publishDate: '2023-05-30T12:57:18.535811', keywords: 'key5', format: 'txt', state: 'state' },
+  ];
+
+
+  const columns: GridColDef[] = [
+    { field: 'name', headerClassName:'header', headerName: 'Name', flex: 1 },
+    {
+      field: 'publishDate',
+      headerClassName:'header',
+      headerName: 'Publish Date',
+      flex: 1,
+      type: 'string',
+      // leaving this valueFormatter for now, until the dataset data formats is known. 
+      /*valueFormatter: (params: GridValueFormatterParams<Date>) => {
+        if (params.value == null) {
+          return '';
+        }
+        return `${params.value.toLocaleDateString('en')} `;
+      }, */
+    },
+    {
+      field: 'keywords',
+      headerClassName:'header',
+      headerName: 'Keywords',
+      flex: 1,
+      type: 'array'
+    },
+    {
+      field: 'format',
+      headerClassName:'header',
+      headerName: 'Format',
+      flex: 1
+    },
+    {
+      field: 'state',
+      headerClassName:'header',
+      headerName: 'State',
+      flex: 1
+    },
+  ];
+
+  return (
+    <DataGridTable columns={columns} rows={rows} error={null} data={null} />
+  );
+};
+
+export default DatasetsTable;
