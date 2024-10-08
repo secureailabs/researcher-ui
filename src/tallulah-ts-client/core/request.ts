05c29519e62b3c0b36602929a40f71d16cfcad4f
Muthu Subramanian <muthu@arrayinsights.com>
2024-10-08 14:43:06 +0530

    feat: client api files update

--- src/tallulah-ts-client/core/request.ts
+++ src/tallulah-ts-client/core/request.ts
@@ -203,7 +203,6 @@ const sendRequest = async <T>(
     } catch (error) {
         const axiosError = error as AxiosError;
         if (axiosError.response) {
-            // @ts-ignore
             return axiosError.response;
         }
         throw error;
